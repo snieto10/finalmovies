7e6904b534b27f18605aec5ecf089fd4e629c40a
Sergio Nieto <snieto10@gmail.com>
2022-09-14 15:44:09 -0400

    Revert "section 4 17"
    This reverts commit 1e3eed12c35f89a6d430492cdd55f2659e0c3a5a.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,21 @@ import Movies from "./components/movies";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notfound";
-import NavBar from "./components/navBar";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <main class="container">
-          <NavBar />
-        </main>
-        <main className="container">
-          <Switch>
-            <Route path="/movies" component={Movies}></Route>
-            <Route path="/customers" component={Customers}></Route>
-            <Route path="/rentals" component={Rentals}></Route>
-            <Route path="/not-found" component={NotFound} />
-            <Redirect from="/" exact to="/movies" />
-            <Redirect to="/not-found" />
-          </Switch>
-        </main>
-      </React.Fragment>
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
     );
   }
 }
