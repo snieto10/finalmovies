9810ef06582ee1512373f880e8813f87131f4572
Sergio Nieto <snieto10@gmail.com>
2022-09-14 12:50:52 -0400

    section 4 16

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notfound";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   }
