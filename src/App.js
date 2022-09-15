00bbc3cdc5c9fbf18278fc0ee594853b0ce942ad
Sergio Nieto <snieto10@gmail.com>
2022-09-14 21:19:04 -0400

    section 6 completed

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import "./App.css";
 import Customers from "./components/customers";
+import NavBar from "./components/navBar";
 
 class App extends Component {
   render() {
     return (
       <main className="container">
+        <NavBar />
         <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
           <Route path="/rentals" component={Rentals} />
---
+++ src/components/navBar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="sergio">
+      <ul className="navbar10">
+        <li className="navbar100">
+          <Link to="/">Vidly</Link>
+        </li>
+        <li className="navbar100">
+          <NavLink to="/movies">Movies</NavLink>
+        </li>
+        <li className="navbar100">
+          <NavLink to="/customers">Customer</NavLink>
+        </li>
+        <li className="navbar100">
+          <NavLink to="/rentals">Rentals</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
